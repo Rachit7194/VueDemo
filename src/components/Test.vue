38d65496fca4bc9aa423e67cf1b0262b910994c2
Rachit <kapadiarachit007@gmail.com>
2018-06-15 18:33:48 +0530

    Demo Practice for VueJS

---
+++ src/components/Test.vue
@@ -0,0 +1,89 @@
+<template>
+  <div class="test">
+    <input type="text" v-model="title" /><br/>
+    <h1>
+      {{title}}
+    </h1>
+    <!-- <span v-html="title"></span> -->
+    <!-- <p>{{user.firstname }}</p> -->
+    <!-- <p>{{user.lastname }}</p> -->
+    <span v-if="showname">{{user.firstname }}</span>
+    <span v-else>Nobody</span>
+    <ul>
+      <li v-for="item in items">
+          {{item.title}}
+      </li>
+    </ul>
+    <button v-on:click="greet('Hello World')">Say Greetings..!!!</button>
+    <br/>
+    <br/>
+    <input type="text" v-on:keyup="PressKey" v-on:keyup.enter="EnterHit"/>
+
+    <hr />
+    <label>First Name :</label><input type="text" v-model="user.firstname"/><br /><br />
+    <label>Last Name :</label><input type="text" v-model="user.lastname"/><br />
+    <h3>
+      {{fullname}}
+    </h3>
+    <h2>
+      {{msg}}
+    </h2>
+    <!-- <input type="text" v-on:keyup="PressKey" v-on:keyup.shift="EnterShift"/> -->
+    <!-- <input type="text" v-on:keyup="PressKey" v-on:keyup.enter="EnterHit"/> -->
+    <!-- <input type="text" v-on:keyup="PressKey" v-on:keyup.enter="EnterHit"/> -->
+  </div>
+</template>
+
+
+<script>
+export default{
+  name : "test",
+  props : {
+    msg : {
+      default : 'Foobar',
+      type: String
+    }
+  },
+  data(){
+    return{
+      title:"Hello Test World",
+      user : {
+        firstname: 'Rachit',
+        lastname: 'Kapadia'
+      },
+      showname : true,
+      items : [
+        {title:"Jay Savsani(BC)"},
+        {title:"Naresh Chaudhary"},
+        {title:"Jay Modi(MC)"}
+      ]
+    }
+  },
+  methods : {
+    greet: function(greeting) {
+      alert(greeting);
+    },
+    PressKey: function(e) {
+      console.log(e.target.value)
+    },
+    EnterHit :function(){
+      console.log("You hit Enter")
+    }
+    // EnterShift: function(){
+    //   console.log("Hit Shift")
+    // }
+
+  },
+  computed : {
+    fullname : function(){
+      return this.user.firstname + " " + this.user.lastname;
+    }
+  }
+}
+</script>
+
+
+
+<style scoped>
+
+</style>
---
+++ src/components/Users.vue
@@ -0,0 +1,84 @@
+<template>
+  <div class="users">
+    <h1>
+      Users
+    </h1>
+    <form v-on:submit="addUser">
+      <label>Name : </label><input type="text" v-model="newUser.name" placeholder="Enter name"/>
+      <br />
+      <br />
+
+      <label>Enter Email : </label><input type="text" v-model="newUser.email" placeholder="Enter Email"/>
+      <br />
+      <br />
+
+      <input type="submit" value="submit" />
+
+    </form>
+
+    <ul>
+      <li v-for="user in users ">
+        <input type="checkbox" v-model="user.contacted"/>
+        <span :class="{contacted: user.contacted}">
+          {{ user.name }}: {{user.email}} <button v-on:click="deleteUser(user)">Del</button>
+        </span>
+
+      </li>
+    </ul>
+
+  </div>
+</template>
+
+
+<script>
+export default{
+  name : "users",
+  data(){
+    return{
+      newUser : {},
+      users : [
+        {
+          name : "Rachit Kapadia",
+          email : "rachit@quixom.com",
+          contacted : false
+        },
+        {
+          name : "Jay Savsani",
+          email : "rachit@quixom.com",
+          contacted : false
+        },
+        {
+          name : "Naresh Chaudhary",
+          email : "rachit@quixom.com",
+          contacted : false
+      }
+      ]
+
+    }
+  },
+  methods : {
+    addUser: function(e){
+      this.users.push(
+        {
+          name : this.newUser.name,
+          email : this.newUser.email,
+          contacted : false
+        }
+      );
+
+      e.preventDefault();
+    },
+    deleteUser: function(){
+      this.users.splice(this.users.indexOf(user,1));
+    }
+  }
+  }
+</script>
+
+
+
+<style scoped>
+.contacted{
+  text-decoration:line-through;
+}
+</style>
